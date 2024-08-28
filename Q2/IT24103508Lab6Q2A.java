57f3b0a02cdd27aef2f532a9854f7e86fcab7a47
IT24103508 <IT24103508@my.sliit.lk>
2024-08-29 02:03:32 +0530

    Add files via upload

---
+++ Q2/IT24103508Lab6Q2A.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class IT24103508Lab6Q2A{
+public static void main(String[]args){
+	int count;
+	count=1;
+	while (count<=10){
+		System.out.print(count+" ");
+		count++;
+		}
+	}
+}
